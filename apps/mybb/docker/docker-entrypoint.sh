abe78a12bb9a98a99e8c7d4827e3d529b6133015
nemanjam <nemanja.mitic.elfak@hotmail.com>
2025-12-29 03:00:33 +0100

    mybb should build now

--- apps/mybb/docker/docker-entrypoint.sh
+++ apps/mybb/docker/docker-entrypoint.sh
@@ -20,13 +20,13 @@ if ! [ -e index.php -a -e inc/class_core.php ]; then
 	echo >&2 "MyBB not found in $PWD - copying now..."
 	if [[ $SKIP_OLD_FILES == true ]]; then
 		echo >&2 "Preserving existing directory files..."
-		tar cf - --one-file-system -C /usr/src/mybb-mybb_${MYBB_VERSION} . | tar xf - --skip-old-files
+		tar cf - --one-file-system -C /usr/src/mybb-${MYBB_VERSION} . | tar xf - --skip-old-files
 	else
 		if [ "$(ls -A)" ]; then
 			echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
 			( set -x; ls -A; sleep 10 )
 		fi
-		tar cf - --one-file-system -C /usr/src/mybb-mybb_${MYBB_VERSION} . | tar xf -
+		tar cf - --one-file-system -C /usr/src/mybb-${MYBB_VERSION} . | tar xf -
 	fi
 	echo >&2 "Complete! MyBB ${MYBB_VERSION} has been successfully copied to $PWD"
 fi
