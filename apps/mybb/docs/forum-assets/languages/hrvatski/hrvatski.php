08615dc55f3d7a175c62407dfa49c72169290bea
nemanjam <nemanja.mitic.elfak@hotmail.com>
2026-01-25 14:29:42 +0100

    add languages assets

---
+++ apps/mybb/docs/forum-assets/languages/hrvatski/hrvatski.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * MyBB 1.8 English Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "Hrvatski (Croatian)";
+
+// The author of the language
+$langinfo['author'] = "Ivan Kerepčić";
+
+// The language authors website
+$langinfo['website'] = "http://ivankerepcic.iz.hr/";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1822";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 0;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "hr";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
